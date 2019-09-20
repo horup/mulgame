f991eeebf7e8583674aa1ed494eb89c743409c79
horup <sah@sah.dk>
2019-09-20 09:20:43 +0200

    added mqtt inbetween

--- src/NetworkManager/index.ts
+++ src/NetworkManager/index.ts
@@ -1,5 +1,8 @@
 import {Msg, Thing} from './Msg';
 import {EventEmitter} from 'events';
+import * as mqtt from 'mqtt';
+import { MqttClient } from 'mqtt';
+
 
 export class GameState
 {
@@ -9,9 +12,30 @@ export class GameState
 
 export class NetworkManager
 {
+    client:MqttClient;
     state = new GameState();
     emitter = new EventEmitter();
     updateFrequency = 2;
+
+    rootTopic = "mulgame/somewhere/abc";
+
+    constructor()
+    {
+        this.client = mqtt.connect("http://broker.mqttdashboard.com:8000/mqtt");
+        this.client.subscribe(this.rootTopic);
+        this.client.on('connect', (e)=>
+        {
+            
+        });
+
+        this.client.on('message', (topic, msg)=>
+        {
+            let s = msg.toString();
+            let o = JSON.parse(s) as Msg;
+            this.emitter.emit('msg', o);
+        });
+    }
+
     on(event:'msg', f:(msg:Msg)=>any)
     {
         this.emitter.addListener(event, f);
@@ -20,7 +44,7 @@ export class NetworkManager
     pushMsg(msg:Msg)
     {
         this.onMsg(msg);
-        this.emitter.emit('msg', msg);
+        this.client.publish(this.rootTopic, JSON.stringify(msg));
     }
 
     onMsg(msg:Msg)
@@ -33,7 +57,5 @@ export class NetworkManager
             let t = msg.setThing;
             s.things[t.id] = t;
         }
-
-        console.log(msg);
     }
 }
